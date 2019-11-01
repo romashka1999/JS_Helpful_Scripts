a55e6288bd0fe95a13d5abda3d5e09ea9868d784
Your Namect git config --global user.email <roma.chikhladze777@gmail.com>
2019-11-01 22:20:34 +0400

    initial commit

---
+++ getAllFilesPath.js
@@ -0,0 +1,34 @@
+const { readdirSync, statSync } = require('fs')
+const { join } = require('path')
+
+const getAllFilesPathSync = (baseUrl, extension = new RegExp('.', 'g'), files = readdirSync(baseUrl), result = []) => {
+
+    if(!baseUrl) {
+        const err = new Error('BaseUrl parameter is required');
+        return err
+    }
+
+    files.forEach( 
+        (file) => {
+            const newBaseUrl = join(baseUrl, file)
+            if ( statSync(newBaseUrl).isDirectory() ) {
+                result = getAllFilesPathSync(newBaseUrl, extension, readdirSync(newBaseUrl), result);
+            } else {
+                file.substr(-1*(extension.length+1)).match(extension) ? result.push(newBaseUrl) : null ;
+            }
+        }
+    )
+
+    return result
+}
+
+/********************** Driver Code *********************/
+console.log(getAllFilesPathSync('./','txt'));
+
+
+
+
+
+
+
+
